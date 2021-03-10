52beecf19a9b36eba1c450d63bd3a40c534ccd48
Fahima345 <71413876+Fahima345@users.noreply.github.com>
2021-03-10 12:59:53 -0500

    resume.java added

---
+++ app/src/main/java/com/example/myportfolio/Resume.java
@@ -0,0 +1,21 @@
+package com.example.myportfolio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class Resume extends AppCompatActivity {
+
+    PDFView pdfView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resume);
+
+        pdfView = findViewById(R.id.pdfViewResume);
+        pdfView.fromAsset("FahimaResume.pdf").load();
+    }
+}
